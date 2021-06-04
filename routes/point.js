3ec9d8edce2ee413f41f2623e008ce3ac8ab0f6b
serapbaysal <serap.baysal@hotmail.com>
2021-06-04 18:02:49 +0300

    updated point

---
+++ routes/point.js
@@ -0,0 +1,16 @@
+const express = require('express');
+/*
+const { getPoint
+} = require('../controllers/points')
+*/
+
+
+const router = express.Router();
+
+
+ 
+//router.get("/:id/Points", getPoint)
+     
+
+  
+module.exports = router;
