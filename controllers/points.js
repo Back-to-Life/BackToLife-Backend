99b63ad67a7e5dcc5e9f32c84b9da3a4aaef5954
serapbaysal <serap.baysal@hotmail.com>
2021-06-14 19:27:35 +0300

    updated points

--- controllers/points.js
+++ controllers/points.js
@@ -5,8 +5,8 @@ const Points = require('../models/Points');
 
 
 // GET
-exports.getPoint = async(req, res, next) => {
-  const points = await Points.findOne({unicID:req.params.id})
+exports.getPoint = async (req, res, next) => {
+  const points = await Points.findOne({ unicID: req.params.id })
   return res.json({
     success: true,
     data: points
@@ -18,176 +18,183 @@ exports.getPoint = async(req, res, next) => {
 exports.updatePoint = async (req, res, next) => {
   pointName = req.body.pointName
 
-  if (pointName == "glass") {
-    const user = await User.findByIdAndUpdate(
 
-      req.params.id,
-      {
-        $inc: {
-          point: 15
+  if (pointName == "glass" || pointName == "plastic" || pointName == "electronic" || pointName == "battery" || pointName == "metal" || pointName == "organic" || pointName == "paper") {
 
+    if (pointName == "glass") {
+      const user = await User.findByIdAndUpdate(
+
+        req.params.id,
+        {
+          $inc: {
+            point: 15
+
+          }
         }
-      }
 
 
-    );
-    const point = await Points.findOneAndUpdate(
-      user.unicID,
-      {
-        $inc: {
-          glass: 1
+      );
+      const point = await Points.findOneAndUpdate(
+        user.unicID,
+        {
+          $inc: {
+            Glass: 1
+          }
         }
-      }
-    )
+      )
 
-    res.status(200).json({ success: true })
+      res.status(200).json({ success: true })
+    }
+    if (pointName == "plastic") {
+      const user = await User.findByIdAndUpdate(
+
+        req.params.id,
+        {
+          $inc: {
+            point: 13
+
+          }
+        }
+
+
+      );
+      const point = await Points.findOneAndUpdate(
+        user.unicID,
+        {
+          $inc: {
+            Plastic: 1
+          }
+        }
+      )
+      res.status(200).json({ success: true })
+    }
+    if (pointName == "electronic") {
+      const user = await User.findByIdAndUpdate(
+
+        req.params.id,
+        {
+          $inc: {
+            point: 11
+
+          }
+        }
+
+
+      );
+      const point = await Points.findOneAndUpdate(
+        user.unicID,
+        {
+          $inc: {
+            Electronic: 1
+          }
+        }
+      )
+      res.status(200).json({ success: true })
+    }
+    if (pointName == "battery") {
+      const user = await User.findByIdAndUpdate(
+
+        req.params.id,
+        {
+          $inc: {
+            point: 9
+
+          }
+        }
+
+
+      );
+      const point = await Points.findOneAndUpdate(
+        user.unicID,
+        {
+          $inc: {
+            Battery: 1
+          }
+        }
+      )
+      res.status(200).json({ success: true })
+    }
+    if (pointName == "metal") {
+      const user = await User.findByIdAndUpdate(
+
+        req.params.id,
+        {
+          $inc: {
+            point: 7
+
+          }
+        }
+
+
+      );
+      const point = await Points.findOneAndUpdate(
+        user.unicID,
+        {
+          $inc: {
+            Metal: 1
+          }
+        }
+      )
+      res.status(200).json({ success: true })
+    }
+    if (pointName == "organic") {
+      const user = await User.findByIdAndUpdate(
+
+        req.params.id,
+        {
+          $inc: {
+            point: 5
+
+          }
+        }
+
+
+      );
+      const point = await Points.findOneAndUpdate(
+        user.unicID,
+        {
+          $inc: {
+            Organic: 1
+          }
+        }
+      )
+      res.status(200).json({ success: true })
+    }
+    if (pointName == "paper") {
+      const user = await User.findByIdAndUpdate(
+
+        req.params.id,
+        {
+          $inc: {
+            point: 3
+
+          }
+        }
+
+
+      );
+      const point = await Points.findOneAndUpdate(
+        user.unicID,
+        {
+          $inc: {
+            Paper: 1
+          }
+        }
+      )
+      res.status(200).json({ success: true })
+    }
+
+
+
+
   }
-  if (pointName == "plastic") {
-    const user = await User.findByIdAndUpdate(
-
-      req.params.id,
-      {
-        $inc: {
-          point: 13
-
-        }
-      }
-
-
-    );
-    const point = await Points.findOneAndUpdate(
-      user.unicID,
-      {
-        $inc: {
-          plastic: 1
-        }
-      }
-    )
-    res.status(200).json({ success: true })
-  }
-  if (pointName == "electronic") {
-    const user = await User.findByIdAndUpdate(
-
-      req.params.id,
-      {
-        $inc: {
-          point: 11
-
-        }
-      }
-
-
-    );
-    const point = await Points.findOneAndUpdate(
-      user.unicID,
-      {
-        $inc: {
-          electronic: 1
-        }
-      }
-    )
-    res.status(200).json({ success: true })
-  }
-  if (pointName == "battery") {
-    const user = await User.findByIdAndUpdate(
-
-      req.params.id,
-      {
-        $inc: {
-          point: 9
-
-        }
-      }
-
-
-    );
-    const point = await Points.findOneAndUpdate(
-      user.unicID,
-      {
-        $inc: {
-          battery: 1
-        }
-      }
-    )
-    res.status(200).json({ success: true })
-  }
-  if (pointName == "metal") {
-    const user = await User.findByIdAndUpdate(
-
-      req.params.id,
-      {
-        $inc: {
-          point: 7
-
-        }
-      }
-
-
-    );
-    const point = await Points.findOneAndUpdate(
-      user.unicID,
-      {
-        $inc: {
-          metal: 1
-        }
-      }
-    )
-    res.status(200).json({ success: true })
-  }
-  if (pointName == "organic") {
-    const user = await User.findByIdAndUpdate(
-
-      req.params.id,
-      {
-        $inc: {
-          point: 5
-
-        }
-      }
-
-
-    );
-    const point = await Points.findOneAndUpdate(
-      user.unicID,
-      {
-        $inc: {
-          organic: 1
-        }
-      }
-    )
-    res.status(200).json({ success: true })
-  }
-  if (pointName == "paper") {
-    const user = await User.findByIdAndUpdate(
-
-      req.params.id,
-      {
-        $inc: {
-          point: 3
-
-        }
-      }
-
-
-    );
-    const point = await Points.findOneAndUpdate(
-      user.unicID,
-      {
-        $inc: {
-          paper: 1
-        }
-      }
-    )
-    res.status(200).json({ success: true })
-  }else {
+  else {
     res.status(400).json({ success: false })
   }
 
 
 
 
-
-  
 
 }
 
